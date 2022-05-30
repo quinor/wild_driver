7b31beda27dbc9b91efd2e54f9c67cbc00de437e
Wojciech Jabłoński <wj359634@students.mimuw.edu.pl>
2022-05-30 16:53:25 +0200

    add color handling

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,47 @@
 #include "external/CLI11.hpp"
 #include "external/linalg.h"
 #define NANOSVG_IMPLEMENTATION
+#define NANOSVG_ALL_COLOR_KEYWORDS
 #include "external/nanosvg.h"
 
 #include <cstdio>
 #include <cmath>
 #include <random>
+#include <unordered_set>
+#include <unordered_map>
 
 using float2 = linalg::vec<float, 2>;
 using mat2 = linalg::mat<float, 2, 2>;
 
+
+
+// my addition to NSVG
+
+typedef union NSVGcolor {
+	unsigned int color;
+	struct {
+		unsigned char r;
+		unsigned char g;
+		unsigned char b;
+		unsigned char a;
+	};
+} NSVGcolor;
+
+
 // 50 steps per mm - as per Wild TA-10's documentation
 const float STEPS_PER_MM = 50;
 
+
+int color_distance (uint32_t c1p, uint32_t c2p)
+{
+    NSVGcolor c1 = {.color=c1p};
+    NSVGcolor c2 = {.color=c2p};
+
+    int dr = c1.r - c2.r;
+    int dg = c1.g - c2.g;
+    int db = c1.b - c2.b;
+    return dr*dr + dg*dg + db*db;
+}
 
 struct Section {
     float2 first;
@@ -218,6 +247,8 @@ int main(int argc, char** argv)
     float scale = 1;
     float2 pre_translate={0,0};
     float2 translate={0,0};
+    std::unordered_set<std::string> color_key_set;
+    bool colors_only = false;
     bool box = false;
     int dry_run = 0;
     bool cut = false;
@@ -225,9 +256,11 @@ int main(int argc, char** argv)
     int speed = 0;
     float min_step_size = 0.5;
     bool order_paths = true;
+    bool no_outline = false;
     bool hatch = false;
     float hatch_density = 0;
     bool visualize = true;
+    bool json = false;
 
     // IO options
     app.add_option("input,-i,--input", in_fname, "Input svg file.")
@@ -236,6 +269,8 @@ int main(int argc, char** argv)
 
     app.add_option("output,-o,--output", out_fname, "Output plotter commands file.")
         ->default_val("out.wild");
+
+    app.add_flag("--json", json, "Instead of regular stdout, output machine-readable json");
 
     // transforms
     app.add_option<float>("--scale", scale, "Scale of the plot.")
@@ -252,6 +287,11 @@ int main(int argc, char** argv)
         ->default_str("0 0");
 
     // plotting options
+    app.add_option<std::unordered_set<std::string>>("--color_key", color_key_set,
+        "Draw only specified colors.");
+
+    app.add_flag("--colors_only", colors_only, "Only extract colors from the svg.");
+
     app.add_flag("--box", box, "Draw the box around the extents of the plot instead. "
         "Forces --dry_run unless otherwise specified.");
 
@@ -278,6 +318,8 @@ int main(int argc, char** argv)
         ->default_val(0.5)
         ->check(CLI::PositiveNumber);
 
+    app.add_flag("--no_outline", no_outline, "Don't trace outlines of shapes");
+
     app.add_flag("--hatch", hatch, "Hatch the inside of (filled in) svg shapes");
 
     app.add_option<float>("--hatch_density", hatch_density, "Distance (in mm) between two consecutive hatch lines.")
@@ -290,6 +332,14 @@ int main(int argc, char** argv)
 
     CLI11_PARSE(app, argc, argv);
 
+    FILE* text_out = stdout;
+    FILE* json_out = fopen("/dev/null", "w");
+
+    bool color_key = color_key_set.size() != 0;
+
+    if (json)
+        std::swap(text_out, json_out);
+
     if (box)
         dry_run++;
 
@@ -299,7 +349,9 @@ int main(int argc, char** argv)
         return (p+pre_translate)*scale + translate;
     };
 
-    printf("parsing input file %s ...\n", in_fname.c_str());
+    fprintf(text_out, "parsing input file %s ...\n", in_fname.c_str());
+
+    fprintf(json_out, "{\n");
 
     // caution. the svg has got x going "right" and y "down". plotters use x "right and y "up", so
     // it has to be compensated for when rasterizing the polyline!
@@ -311,23 +363,90 @@ int main(int argc, char** argv)
 
     float2 beg = transform({0,0}), end = transform(svg_size);
 
-    printf("input image of size %.2fmm by %.2fmm\n", svg_size.x, svg_size.y);
-    printf("transformed extents are:\n    x: (%.2fmm, %.2fmm)\n    y: (%.2fmm, %.2fmm)\n",
+    fprintf(text_out, "input image of size %.2fmm by %.2fmm\n", svg_size.x, svg_size.y);
+    fprintf(text_out, "transformed extents are:\n    x: (%.2fmm, %.2fmm)\n    y: (%.2fmm, %.2fmm)\n",
         beg.x, end.x, beg.y, end.y);
+
+    fprintf(json_out, "    \"input_size\": { \"x\": %.2f, \"y\": %.2f },\n", svg_size.x, svg_size.y);
+    fprintf(json_out,
+        "    \"transformed_size\": { "
+        "\"begin\": { \"x\": %.2f, \"y\": %.2f }, "
+        "\"end\": { \"x\": %.2f, \"y\": %.2f } },\n",
+        beg.x, beg.y, end.x, end.y
+    );
+
 
     if (beg.x < 0 || beg.y < 0 || end.x < 0 || end.y < 0)
     {
-        printf("Error! Attempting to draw in area with negative coordinates. "
-            "Please translate the plot.");
+        fprintf(text_out, "Error! Attempting to draw in area with negative coordinates. "
+            "Please translate the plot.\n");
+        fprintf(json_out, "    \"error\": \"Attempting to draw in area with negative coordinates!\"\n}\n");
         return -1;
     }
+
+    std::unordered_set<uint32_t> all_colors;
+    std::unordered_map<uint32_t, NSVGNamedColor> color_names;
+    std::unordered_map<uint32_t, bool> color_enabled;
+
+    // gather all colors
+    for (NSVGshape *shape = svg->shapes; shape != NULL; shape = shape->next)
+    {
+        if (!(shape->flags & NSVG_FLAGS_VISIBLE))
+            continue;
+        if (shape->stroke.type == NSVG_PAINT_COLOR)
+            all_colors.insert(shape->stroke.color);
+        if (shape->fill.type == NSVG_PAINT_COLOR)
+            all_colors.insert(shape->fill.color);
+    }
+
+    fprintf(text_out, "color names detected from the image:\n");
+    fprintf(json_out, "    \"colors\": [");
+    size_t ncolors = sizeof(nsvg__colors)/sizeof(NSVGNamedColor);
+    bool first = true;
+    for (auto color : all_colors)
+    {
+        NSVGNamedColor best = nsvg__colors[0];
+        for (size_t i=0; i<ncolors; i++)
+        {
+            NSVGNamedColor cur = nsvg__colors[i];
+            if (color_distance(color, best.color) > color_distance(color, cur.color))
+                best = cur;
+        }
+        color_names[color] = best;
+        color_enabled[color] = color_key_set.count(best.name);
+        NSVGcolor unpacked = {.color=color};
+        fprintf(text_out, "    rgb(%3d, %3d, %3d) #%02x%02x%02x %s\n",
+            unpacked.r, unpacked.g, unpacked.b,
+            unpacked.r, unpacked.g, unpacked.b,
+            best.name
+        );
+        fprintf(json_out, "%s\n", first ? "" : ",");
+        first = false;
+        fprintf(json_out,
+            "        { \"rgb\": { \"r\": %d, \"g\": %d, \"b\": %d }, "
+            "\"hex\": \"#%02x%02x%02x\", \"name\": \"%s\"}",
+            unpacked.r, unpacked.g, unpacked.b,
+            unpacked.r, unpacked.g, unpacked.b,
+            best.name
+        );
+    }
+
+    if (colors_only)
+    {
+        fprintf(json_out, "\n    ]\n");
+        fprintf(json_out, "}\n");
+        return 0;
+    }
+    else
+        fprintf(json_out, "\n    ],\n");
 
     std::vector<Path> paths;
 
     // draw the box in the box mode
     if (box)
     {
-        printf("drawing the box outline of the plot only\n");
+        fprintf(text_out, "drawing the box outline of the plot only\n");
+        fprintf(json_out, "    \"box\": true,\n");
         paths.emplace_back();
         std::vector<float2>& pln = paths.back().pts;
 
@@ -340,6 +459,9 @@ int main(int argc, char** argv)
     // otherwise rasterize the svg
     else
     {
+        fprintf(text_out, "rasterizing plot%s...\n", hatch ? " and hatching" : "");
+        fprintf(json_out, "    \"hatch\": %s,\n", hatch ? "true" : "false");
+
         std::function<void(std::vector<float2>&, float2, float2, float2, float2, int)> rasterize_bezier =
         [&](std::vector<float2>& pln, float2 a, float2 b, float2 c, float2 d, int n) -> void
         {
@@ -362,8 +484,6 @@ int main(int argc, char** argv)
             rasterize_bezier(pln, x, l, r, d, n+1);
         };
 
-
-        printf("rasterizing plot%s...\n", hatch ? " and hatching" : "");
         for (NSVGshape *shape = svg->shapes; shape != NULL; shape = shape->next)
         {
             if (!(shape->flags & NSVG_FLAGS_VISIBLE))
@@ -371,27 +491,36 @@ int main(int argc, char** argv)
 
             size_t idx_beg = paths.size();
 
-            for (NSVGpath *path = shape->paths; path != NULL; path = path->next)
+            unsigned int outline_color =
+                shape->stroke.type == NSVG_PAINT_COLOR
+                ? shape->stroke.color
+                : shape->fill.color;
+
+            if (!no_outline && (!color_key || color_enabled[outline_color]))
             {
-                paths.emplace_back();
-                std::vector<float2>& pln = paths.back().pts;
-
-                for (int n = 0; n < path->npts-1; n += 3)
+                for (NSVGpath *path = shape->paths; path != NULL; path = path->next)
                 {
-                    float* pt = &path->pts[n*2];
-                    float2 a = {pt[0], svg_size.y-pt[1]};
-                    float2 b = {pt[2], svg_size.y-pt[3]};
-                    float2 c = {pt[4], svg_size.y-pt[5]};
-                    float2 d = {pt[6], svg_size.y-pt[7]};
+                    paths.emplace_back();
+                    std::vector<float2>& pln = paths.back().pts;
 
-                    if (n == 0)
-                        pln.emplace_back(transform(a));
-                    rasterize_bezier(pln, a, b, c, d, 0);
-                    pln.emplace_back(transform(d));
+                    for (int n = 0; n < path->npts-1; n += 3)
+                    {
+                        float* pt = &path->pts[n*2];
+                        float2 a = {pt[0], svg_size.y-pt[1]};
+                        float2 b = {pt[2], svg_size.y-pt[3]};
+                        float2 c = {pt[4], svg_size.y-pt[5]};
+                        float2 d = {pt[6], svg_size.y-pt[7]};
+
+                        if (n == 0)
+                            pln.emplace_back(transform(a));
+                        rasterize_bezier(pln, a, b, c, d, 0);
+                        pln.emplace_back(transform(d));
+                    }
                 }
             }
 
-            if (shape->fill.type == NSVG_PAINT_NONE || !hatch)
+            unsigned int fill_color = shape->fill.color;
+            if (shape->fill.type != NSVG_PAINT_COLOR || !hatch || (color_key && !color_enabled[fill_color]))
                 continue;
 
             size_t idx_end = paths.size();
@@ -403,9 +532,16 @@ int main(int argc, char** argv)
 
     nsvgDelete(svg);
 
+    if (paths.size() == 0)
+    {
+        fprintf(text_out, "Error! Zero paths rendered!\n");
+        fprintf(json_out, "    \"error\": \"Zero paths rendered!\"\n}\n");
+        return -1;
+    }
+
     if (order_paths)
     {
-        printf("reordering paths...\n");
+        fprintf(text_out, "reordering paths...\n");
         // intentionally ignore the copy from paths to new_paths and then back
         // it doesn't matter too much performance-wise. TODO: maybe fix.
         std::vector<Path> new_paths;
@@ -437,7 +573,7 @@ int main(int argc, char** argv)
 
     if (visualize)
     {
-        printf("saving visualization as vis.svg\n");
+        fprintf(text_out, "saving visualization as vis.svg\n");
         FILE* vis = fopen("vis.svg", "w");
 
         float2 size = (end-beg);
@@ -492,7 +628,7 @@ int main(int argc, char** argv)
     FILE* output = fopen(out_fname.c_str(), "w");
     if (output == nullptr)
     {
-        printf("could not open output file: %s\n", out_fname.c_str());
+        fprintf(text_out, "could not open output file: %s\n", out_fname.c_str());
         return -1;
     }
 
@@ -514,9 +650,11 @@ int main(int argc, char** argv)
     // speed selection. Default of 120mm/s for cutting and 300mm/s (max) for drawing.
     fprintf(output, ":7%d\r", speed == 0 ? (cut ? 15 : 37) : speed);
 
-    printf("using the %s\n", cut ? "cutter" : "pen");
-    printf("the tool is %s\n", dry_run ? "lifted" : "lowered");
-    printf("number of paths rendered: %lu\n", paths.size());
+    fprintf(text_out, "using the %s\n", cut ? "cutter" : "pen");
+    fprintf(text_out, "the tool is %s\n", dry_run ? "lifted" : "lowered");
+
+    fprintf(json_out, "    \"cut\": %s,\n", cut ? "true" : "false");
+    fprintf(json_out, "    \"dry_run\": %s,\n", dry_run ? "true" : "false");
 
     float2 cur = {0,0};
     int lastx = 0, lasty = 0;
@@ -571,11 +709,20 @@ int main(int argc, char** argv)
 
     fclose(output);
 
-    printf("done!\n");
-    printf("stats:\n");
-    printf("    points count: %d\n", points_count);
-    printf("    total path length: %.2fmm\n", total_down);
-    printf("    total free travel length: %.2fmm\n", total_up);
+    fprintf(json_out, "    \"stats\": {\n");
+    fprintf(json_out, "        \"paths\": %lu,\n", paths.size());
+    fprintf(json_out, "        \"points\": %d,\n", points_count);
+    fprintf(json_out, "        \"length_down\": %.2f,\n", total_down);
+    fprintf(json_out, "        \"length_up\": %.2f\n", total_up);
+    fprintf(json_out, "    }\n");
+    fprintf(json_out, "}\n");
+
+    fprintf(text_out, "done!\n");
+    fprintf(text_out, "stats:\n");
+    fprintf(text_out, "    paths count: %lu\n", paths.size());
+    fprintf(text_out, "    points count: %d\n", points_count);
+    fprintf(text_out, "    total path length: %.2fmm\n", total_down);
+    fprintf(text_out, "    total free travel length: %.2fmm\n", total_up);
 
     return 0;
 }
