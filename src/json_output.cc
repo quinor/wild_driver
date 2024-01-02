8ddde817f002b4a9ecedeec49fbcdf07218abd54
Wojciech Jablonski <wojciech.maciej.jablonski@gmail.com>
2024-01-02 02:25:00 +0100

    Improve json output format

--- src/json_output.cc
+++ src/json_output.cc
@@ -9,7 +9,8 @@ json& json_out()
     static std::unique_ptr<json, std::function<void(json*)>> out(
         new json({
             {"success", true},
-            {"log", json::array()}
+            {"log", json::array()},
+            {"output", json::object()}
         }),
         [](json* j) -> void
         {
@@ -39,11 +40,11 @@ void json_log_raw(const json& message)
 
 void json_output(const std::string& key, const json& message)
 {
-    if (json_out().contains(key))
+    if (json_out()["output"].contains(key))
         json_fail("`json_output`: the key already exists!", {
             {"key", key},
             {"message", message}
         });
 
-    json_out()[key] = message;
+    json_out()["output"][key] = message;
 }
